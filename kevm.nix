04f07655a860389f0a5a87e52094774e2fd3ac54
Kris Jenkins <krisajenkins@gmail.com>
2018-05-02 12:10:35 +0100

    Adding a nix recipe for building KEVM.

--- kevm.nix
+++ kevm.nix
@@ -3,8 +3,8 @@
 , git
 , openjdk8
 , pandoc
-, ocaml
 , opam
+, ocaml
 , maven
 , z3
 , mpfr
@@ -20,6 +20,8 @@
 , which
 , pkgconfig
 , flex
+, python3
+, zlib
 }:
 
 let evmSemanticsSrc = fetchgit {
@@ -27,49 +29,20 @@ let evmSemanticsSrc = fetchgit {
       rev = "d9e8102051b99141ca5ad8d33227304225647aa7";
       sha256 = "0z7y1snz49085p2c3kih01yr5j2sxis7wkgxdcmsbakj2lbq1di1";
     };
-
-    # Pre-load the maven dependencies. They don't change much and it takes ages to download.
-    mavenDeps = stdenv.mkDerivation {
-      name = "kevmMavenDeps";
-      src = evmSemanticsSrc;
-      buildInputs = [ maven ];
-      buildPhase = ''
-        mkdir -p $out/.m2
-        cd .build/k
-        mvn -Dmaven.repo.local=$out/.m2 dependency:resolve
-      '';
-      installPhase = ''
-        echo
-      '';
-    };
 in stdenv.mkDerivation {
-  src = evmSemanticsSrc;
   name = "kevm";
-  buildInputs = [ git pandoc openjdk8 ocaml opam maven z3 mpfr autoconf automake libtool ncurses unzip curl rsync gcc perl which pkgconfig flex ];
+  src = evmSemanticsSrc;
+  patches = [ ./kevm.patch ];
+  buildInputs = [ pandoc openjdk8 ocaml opam maven z3 mpfr autoconf automake libtool ncurses unzip curl rsync gcc perl which pkgconfig flex zlib python3 ];
   buildPhase = ''
     export HOME=$PWD
-
-    cp -pr ${mavenDeps}/.m2 $HOME
-    chmod -R u+w $HOME/.m2
-    substituteInPlace Makefile --replace "mvn" "mvn -Dmaven.repo.local=$HOME/.m2 -Ddependency-check.skip=true"
-
-    substituteInPlace Makefile --replace "git submodule" "echo git submodule"
-
-    substituteInPlace Makefile --replace "k-distribution/target/release/k/lib/opam" "k-distribution/src/main/scripts/lib/opam"
-
-    # mkdir .build/ocaml
-    # export OPAMROOT=opam
-    # mkdir $OPAMROOT
-    # opam init
-    # # $(opam config env)
 
     make deps
     make
   '';
-
   installPhase = ''
-    cp -pr ~/.opam $out/
-    cp -pr .build/local/lib/* $out/
+    mkdir -p $out/bin
+    cp -pr .build/local/lib $out/
     cp -pr .build/vm/* $out/bin/
   '';
 }
